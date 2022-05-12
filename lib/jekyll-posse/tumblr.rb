6bc6b5c7910306eaaa6949da0c45568896d2dd2a
Joe Buhlig <joe@joebuhlig.com>
2022-05-12 11:20:09 -0500

    change token assignment method

--- lib/jekyll-posse/tumblr.rb
+++ lib/jekyll-posse/tumblr.rb
@@ -7,15 +7,15 @@ module JekyllPosse
       @data = data
       @content = content
       @blog = blog
-      @token = ENV["TUMBLR_BEARER_TOKEN"]
+      @token = get_bearer_token
       puts @token
     end
 
     def notes
       payload = {"body": @content}
-      info = RestClient.get "https://api.tumblr.com/v2/user/info/", {:Authorization => "Bearer #{@token}"}
+      info = RestClient.get "https://api.tumblr.com/v2/user/info/", {:Authorization => @token}
       puts info
-      post = RestClient.post "https://api.tumblr.com/v2/blog/#{@blog}/post", payload.to_json, {:content_type => :json, :Authorization => "Bearer #{@token}"}
+      post = RestClient.post "https://api.tumblr.com/v2/blog/#{@blog}/post", payload.to_json, {:content_type => :json, :Authorization => @token}
       puts post
       format_post(post.body)
     end
@@ -39,6 +39,11 @@ module JekyllPosse
     def format_post(post)
       post = JSON.parse(post)
       return "https://#{@blog}/#{post["response"]["id_string"]}/"
+    end
+
+    def get_bearer_token()
+      token = ENV["TUMBLR_BEARER_TOKEN"]
+      return "Bearer #{token}"
     end
 
   end
