d1970b027b96919d70c4f4d1bea72d7208c066b0
Joe Buhlig <joe@joebuhlig.com>
2022-05-16 11:16:33 -0500

    append shortlink if enabled

--- lib/jekyll-posse/syndicate.rb
+++ lib/jekyll-posse/syndicate.rb
@@ -12,48 +12,50 @@ module JekyllPosse
   class Syndicate
 
     def self.process
-      jekyll_conf = Jekyll.configuration.dup
-      @posse_conf = jekyll_conf["jekyll_posse"]
-      @posse_conf["collections"].each do |collection|
-        if jekyll_conf["collections"][collection]["output"]
-          Dir["_#{collection}/*.md"].each do |file|
-            content = File.read(file)
-
-            if content =~ Jekyll::Document::YAML_FRONT_MATTER_REGEXP
-              content = $POSTMATCH
-
-              match = Regexp.last_match[1] if Regexp.last_match
-              data = Psych.load(match)
-              data["syndication"] = [] unless data.include?("syndication")
-
-              if data["mp-syndicate-to"] and data["date"] < Time.now
-                if data["mp-syndicate-to"].kind_of?(Array)
-                  data["mp-syndicate-to"].each_with_index do |silo, index|
-                    syndication_url = mp_syndicate(collection, data, content, silo)
-                    data["syndication"].push(syndication_url)
-                    data["mp-syndicate-to"].slice!(index)
-                    puts "Syndicated: #{syndication_url}"
-                  end
-                else
-                  silo = data["mp-syndicate-to"]
-                  syndication_url = mp_syndicate(collection, data, content, silo)
-                  data["syndication"][0] = syndication_url
-                  data["mp-syndicate-to"] = ""
+      @jekyll_conf = Jekyll.configuration.dup
+      @posse_conf = @jekyll_conf["jekyll_posse"]
+      site = Jekyll::Site.new(@jekyll_conf)
+      site.read
+      @posse_conf["collections"].each do |name|
+        collection = Jekyll::Collection.new(site, name)
+        collection.read
+        if collection.metadata["output"]
+          collection.docs.each do |post|
+            post.read
+            data = post.data
+            data["syndication"] = [] unless data.include?("syndication")
+            if data["mp-syndicate-to"] and data["date"] < Time.now
+              if data["mp-syndicate-to"].kind_of?(Array)
+                data["mp-syndicate-to"].each do |silo|
+                  syndication_url = mp_syndicate(post, silo)
+                  data["syndication"].push(syndication_url)
+                  data["mp-syndicate-to"].delete(silo)
                   puts "Syndicated: #{syndication_url}"
                 end
+              else
+                silo = data["mp-syndicate-to"]
+                syndication_url = mp_syndicate(post, silo)
+                data["syndication"][0] = syndication_url
+                data["mp-syndicate-to"] = ""
+                puts "Syndicated: #{syndication_url}"
               end
-
               data.delete("mp-syndicate-to") if (data["mp-syndicate-to"] == [])
-              File.write(file, "#{Psych.dump(data)}---\n#{content}")
+              File.write(post.path, "#{Psych.dump(data)}---\n#{post.content}")
             end
+
           end
         end
       end
     end
 
-    def self.mp_syndicate(collection, data, content, silo)
+    def self.mp_syndicate(post, silo)
       service = @posse_conf["mp-syndicate-to"][silo]
       puts "Syndicating to #{silo}"
+
+      if @posse_conf["permashortlink"]
+        domain = @posse_conf["permashortlink"]["domain"]
+        content = insert_shortlink(post, domain)
+      end
       rendered = Kramdown::Document.new(content).to_html
       sanitized = Sanitize.fragment(rendered)
 
@@ -71,6 +73,22 @@ module JekyllPosse
       else
 
       end
+    end
+
+    def self.insert_shortlink(post, domain)
+      template = @jekyll_conf["jekyll_shorten"][post.type.to_s]["permashortlink"]
+      shortlink = Jekyll::URL.new(
+        :template => template,
+        :placeholders => post.url_placeholders
+      )
+      lines = post.content.split("\n\n")
+      lines.to_enum.with_index.reverse_each do |line, index|
+        unless line.match?(/(^<http.*>$|^\[.*\]\(http.*\)$)/)
+          lines[index] = line + " (<#{domain}#{shortlink}>)"
+          break
+        end
+      end
+      lines.join("\n\n")
     end
 
   end
